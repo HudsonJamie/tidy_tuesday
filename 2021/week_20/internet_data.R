fd2c4422b91ec879075e11d7c2e74d5ebebc1ae9
Jamie Hudson <jh25g11@gmail.com>
2021-05-14 15:04:32 +0100

    Updated figure

--- 2021/week_20/internet_data.R
+++ 2021/week_20/internet_data.R
@@ -44,13 +44,13 @@ broadbands <- broadband %>%
 # plot legend
 
 legend <- ggplot() +
-  geom_circle(aes(x0 = 0, y0 = 1, r = .8), fill = "green") +
+  geom_circle(aes(x0 = 0, y0 = 1, r = .8), fill = "green", colour = "grey15") +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 1.5, r = 2.5, start = 0.8,
-                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm'), colour = "grey15") +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 3.5, r = 4.5, start = 0.8,
-                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm'), colour = "grey15") +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 5.5, r = 6.5, start = 0.8,
-                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm'), colour = "grey15") +
   annotate("text", x = 2.2, y = 1, size = 5, color = "white",
            label = ">15%",
            family = "Roboto Mono") +
@@ -77,31 +77,30 @@ legend <- ggplot() +
 # main plot
 
 map <- ggplot(broadbands) +
-  geom_circle(aes(x0 = 0, y0 = 1, r = 0.9,  fill = circle)) +
+  geom_circle(aes(x0 = 0, y0 = 1, r = 0.9,  fill = circle), colour = "grey15") +
   lims(x = c(-5.5, 5.5),
        y = c(0, 8.5)) +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 1.5, r = 2.5, start = 0.8,
-                   end =  -0.8, fill = bar_one), radius = 0.039) +
+                   end =  -0.8, fill = bar_one), radius = 0.039, colour = "grey15") +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 3.5, r = 4.5, start = 0.8,
-                   end =  -0.8, fill = bar_two), radius = 0.039) +
+                   end =  -0.8, fill = bar_two), radius = 0.039, colour = "grey15") +
   geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 5.5, r = 6.5, start = 0.8,
-                   end =  -0.8, fill = bar_three), radius = 0.039) +
+                   end =  -0.8, fill = bar_three), radius = 0.039, colour = "grey15") +
   scale_fill_identity() +
   coord_fixed() +
   labs(title = "US Broadband Speeds",
        subtitle = "\nThe percentage of residents in each state that \nhave access to fixed terrestrial broadband \nspeeds of 25 Mbps/3 Mbps\n \n",
        caption = "@jamiebio | source: Microsoft") +
-  theme(plot.title = element_text(colour = "white", size = 50,
-                                  family = "Roboto Mono"),
-        plot.subtitle = element_text(colour = "white", size = 20,
-                                     family = "Roboto Mono"),
+  theme(text = element_text(family = "Roboto Mono"),
+        plot.title = element_text(colour = "white", size = 50),
+        plot.subtitle = element_text(colour = "white", size = 20),
+        plot.caption = element_text(colour = "white"),
         plot.background = element_rect(fill = "grey15"),
         panel.background = element_rect(fill = "grey15"),
         axis.title = element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
-        strip.text = element_text(color = "white", size = 15,
-                                  family = "Roboto Mono"),
+        strip.text = element_text(color = "white", size = 15),
         strip.background = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -113,6 +112,6 @@ ggdraw(map) +
 
 ggsave(paste0("internet_data", format(Sys.time(), "%d%m%Y"), ".png"),
        dpi = 320,
-       width = 12.2,
+       width = 12.25,
        height = 11.15,
        type = "cairo-png")
