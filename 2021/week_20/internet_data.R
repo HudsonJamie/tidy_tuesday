30ff60c3085c1440e70205f32ce30ce2a0f046a1
Jamie Hudson <jh25g11@gmail.com>
2021-05-14 14:49:50 +0100

    Added week_20

---
+++ 2021/week_20/internet_data.R
@@ -0,0 +1,121 @@
+# internet_Data.R
+# Jamie Hudson
+# Created: 14 May 2021
+# Edited: 14 May 2021
+# Data: Microsoft (https://github.com/microsoft/USBroadbandUsagePercentages)
+
+# load libraries ----------------------------------------------------------
+
+library(tidytuesdayR)
+library(tidyverse)
+library(zipcodeR)
+library(cowplot)
+library(usdata)
+library(janitor)
+library(ggforce)
+library(geofacet)
+library(showtext)
+
+# load data ---------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load(2021, week = 20)
+
+broadband <- tuesdata$broadband_zip
+
+broadband$state <- abbr2state(broadband$ST)
+
+# wrangle data ------------------------------------------------------------
+
+broadbands <- broadband %>% 
+  group_by(state) %>% 
+  clean_names() %>% 
+  summarise(broadband_usage = mean(as.numeric(broadband_usage), na.rm = T)) %>% 
+  mutate(circle = case_when(broadband_usage >= 0.15 ~ "green",
+                            broadband_usage < 0.15 ~ "grey60"),
+         bar_one = case_when(broadband_usage >= 0.30 ~ "green",
+                             broadband_usage < 0.30 ~ "grey60"),
+         bar_two = case_when(broadband_usage >= 0.45 ~ "green",
+                             broadband_usage < 0.45 ~ "grey60"),
+         bar_three = case_when(broadband_usage >= 0.60 ~ "green",
+                             broadband_usage < 0.60 ~ "grey60"))
+
+# plots -------------------------------------------------------------------
+
+# plot legend
+
+legend <- ggplot() +
+  geom_circle(aes(x0 = 0, y0 = 1, r = .8), fill = "green") +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 1.5, r = 2.5, start = 0.8,
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 3.5, r = 4.5, start = 0.8,
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 5.5, r = 6.5, start = 0.8,
+                   end =  -0.8), fill = "green", radius = unit(2.5, 'mm')) +
+  annotate("text", x = 2.2, y = 1, size = 5, color = "white",
+           label = ">15%",
+           family = "Roboto Mono") +
+  annotate("text", x = 3, y = 2.6, size = 5, color = "white",
+           label = ">30%",
+           family = "Roboto Mono") +
+  annotate("text", x = 4.4, y = 4, size = 5, color = "white",
+           label = ">45%",
+           family = "Roboto Mono") +
+  annotate("text", x = 6.1, y = 6, size = 5, color = "white",
+           label = ">60%",
+           family = "Roboto Mono") +
+  coord_fixed() +
+  lims(x = c(-4.7, 9)) +
+  theme(plot.title = element_text(colour = "white"),
+        plot.background = element_blank(),
+        panel.background = element_rect(fill = "grey5"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+
+# main plot
+
+map <- ggplot(broadbands) +
+  geom_circle(aes(x0 = 0, y0 = 1, r = 0.9,  fill = circle)) +
+  lims(x = c(-5.5, 5.5),
+       y = c(0, 8.5)) +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 1.5, r = 2.5, start = 0.8,
+                   end =  -0.8, fill = bar_one), radius = 0.039) +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 3.5, r = 4.5, start = 0.8,
+                   end =  -0.8, fill = bar_two), radius = 0.039) +
+  geom_arc_bar(aes(x0 = 0, y0 = 1, r0 = 5.5, r = 6.5, start = 0.8,
+                   end =  -0.8, fill = bar_three), radius = 0.039) +
+  scale_fill_identity() +
+  coord_fixed() +
+  labs(title = "US Broadband Speeds",
+       subtitle = "\nThe percentage of residents in each state that \nhave access to fixed terrestrial broadband \nspeeds of 25 Mbps/3 Mbps\n \n",
+       caption = "@jamiebio | source: Microsoft") +
+  theme(plot.title = element_text(colour = "white", size = 50,
+                                  family = "Roboto Mono"),
+        plot.subtitle = element_text(colour = "white", size = 20,
+                                     family = "Roboto Mono"),
+        plot.background = element_rect(fill = "grey5"),
+        panel.background = element_rect(fill = "grey5"),
+        axis.title = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(color = "white", size = 15,
+                                  family = "Roboto Mono"),
+        strip.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = margin(20,50,20,50)) +
+  facet_geo(~ state, grid = "us_state_grid2", label = "code")
+
+ggdraw(map) +
+  draw_plot(legend, x = 0.40, y = 0.80, 0.65, 0.17)
+
+ggdraw(map) +
+  draw_plot(legend, x = 0.55, y = 0.8, 0.65, 0.17)
+
+ggsave(paste0("internet_data", format(Sys.time(), "%d%m%Y"), ".png"),
+       dpi = 320,
+       width = 12.5,
+       height = 11.2,
+       type = "cairo-png")
